e5b923d7f755c0e5ff3d9595a48fe07f7394c129
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-10-11 18:18:55 +0800

    React studium
    React studium

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from '@reach/router';
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+
 
 const App = () => {
   // return React.createElement("div", { id: "something-important"}, [
@@ -20,8 +23,20 @@ const App = () => {
   return (
     <React.StrictMode>
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
+
+        <header>
+          <Link to="/">
+          Adopt Me!
+          </Link>
+        </header>
+        {/* <h1 id="something-important"></h1> */}
+      
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
+
+
     </div>
     </React.StrictMode>
   );
