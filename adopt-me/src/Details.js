64935ca12ce190847bfb968dc7201ce21aa4bfba
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-10-13 23:05:53 +0800

    React studiumsvorgang pausen
    React Studiumsvorgang pausen

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Details = (props) => {
-return <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
+// const Details = (props) => {
+// return <pre><code>{JSON.stringify(props, null, 4)}</code></pre>
+// }
+
+class Details extends React.Component {
+    
+    render () {
+
+    }
 }
 
 export default Details;
