e382b5c911acb923e627b7f90cb534e63a0589d8
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-10-21 10:46:50 +0800

    Bauklotz hinzufügen
    Bauklotz hinzufügen

---
+++ micellaneous/web-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('message', (ev) => {
+    console.log('Web worker started with data: ', ev.data);
+    let data = ev.data;
+    switch(data) {
+        case 'Get started':
+            self.postMessage('Web Worker Started');
+            break;
+        case 'Other':
+            self.postMessage('Other task...');
+            break;
+        case 'Data':
+            let url = 'https://jsonplaceholder.typicode.com/posts';
+            
+            break;
+        default: 
+            console.log('Invalid access');
+            self.postMessage('Closing Web Worker');
+            self.close();
+
+    }
+})
