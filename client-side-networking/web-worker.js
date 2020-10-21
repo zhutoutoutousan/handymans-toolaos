95f362f511b0008748204113a387308de6452399
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-10-21 13:15:16 +0800

    Aktualisierung
    Aktualisierung

--- client-side-networking/web-worker.js
+++ client-side-networking/web-worker.js
@@ -1,16 +1,23 @@
 self.addEventListener('message', (ev) => {
     console.log('Web worker started with data: ', ev.data);
     let data = ev.data;
+console.log(data);
     switch(data) {
-        case 'Get Started':
-            self.postMessage('Web Worker Started');
-            break;
-        case 'Other':
-            self.postMessage('Other task...');
-            break;
-        case 'Data':
+//         case 'Get Started':
+// console.log(self);
+//             self.postMessage('Web Worker Started');
+//             break;
+//         case 'Other':
+//             self.postMessage('Other task...');
+//             break;
+        case 'fetch':
             let url = 'https://jsonplaceholder.typicode.com/posts';
-
+            fetch(url)
+                .then(res => res.json())
+                .then( data => {
+                    self.postMessage( data ); 
+                })
+                .catch(err => console.log(err));
             break;
         default: 
             console.log(data);
