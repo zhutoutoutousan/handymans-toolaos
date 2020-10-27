0c6eeb5b26a92fd4632fa8697de07eb37bfe280e
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-10-27 17:40:47 +0800

    Aktualisierung
    Aktualisierung

---
+++ three-js-project-refactor/js/index.js
@@ -0,0 +1,3 @@
+import * as THREE from 'https://unpkg.com/three@0.121.1/build/three.module.js';
+import { GLTFLoader } as GLTFLoader from 'https://unpkg.com/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
+import { FirstPersonControls } from 'https://unpkg.com/three@0.121.1/examples/jsm/controls/FirstPersonControls.js';
