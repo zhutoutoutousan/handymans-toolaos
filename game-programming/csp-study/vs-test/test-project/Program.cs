0165e1993544a3b3aa248633a21ae53cd815ac37
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2020-12-07 17:40:16 +0800

    Aktualisierung
    Aktualisierung

--- game-programming/csp-study/vs-test/test-project/Program.cs
+++ game-programming/csp-study/vs-test/test-project/Program.cs
@@ -11,8 +11,13 @@ namespace test_project
 {
     class Program
     {
+        // You define enumerations directly in the namespace
+        enum DaysOfWeek { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday};
+        DaysOfWeek today; // Indicate the type, and give it a name.
         static void Main(string[] args)
         {
+            
+
             Console.WriteLine("Hello World");
             Console.ReadKey();
         }
