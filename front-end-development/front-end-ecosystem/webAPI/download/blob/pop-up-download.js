0ce5e27bdc2f52eed3c87a10c9b42609fdbf4d7d
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2021-01-18 16:01:43 +0800

    Aktualisierung
    Aktualisierung

--- front-end-development/front-end-ecosystem/webAPI/download/blob/pop-up-download.js
+++ front-end-development/front-end-ecosystem/webAPI/download/blob/pop-up-download.js
@@ -1,6 +1,10 @@
 // The goal is to provide an entry for Unity WebGL application toggling download from the 
 // brower side
 
+
+
+
+
 // LocalStorage
 
 
---
+++ front-end-development/front-end-ecosystem/webAPI/storage/localStorage.js
@@ -0,0 +1,5 @@
+class storageManagement {
+    constructor() {
+
+    }
+}
