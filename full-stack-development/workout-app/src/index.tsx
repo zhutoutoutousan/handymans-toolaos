e2fd761d096c43a4b970fe710049458b7d016d2b
zhutoutoutousan <mxyuiiu64545@126.com>
2021-03-10 09:16:00 +0800

    Aktualisierung

--- full-stack-development/workout-app/src/index.tsx
+++ full-stack-development/workout-app/src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { AppRegistry } from 'react-native';
+import App from './App';
+import './index.css';
 
+
+// register the app
 AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  initialProps: {},
+  rootTag: document.getElementById('root')
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,4 +24,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+ServiceWorker.unregister();
