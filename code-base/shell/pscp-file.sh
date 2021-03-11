6729a9925b5ecd2c35cc980682698d02b51db59d
zhutoutoutousan <mxyuiiu64545@126.com>
2021-03-12 01:14:19 +0800

    Aktualisierung

--- code-base/shell/pscp-file.sh
+++ code-base/shell/pscp-file.sh
@@ -16,3 +16,5 @@ read TARGETUSER
 echo Copying $FROMDIRECTORY from this machine to $TARGETUSER@$TARGETIP:$TARGETDIRECTORY
 
 pscp -P 22 ${FROMDIRECTORY} $TARGETUSER@$TARGETIP:$TARGETDIRECTORY
+
+# pscp -P 22 E:\_SHARE_WITH_SERVER\to-server\dormitary-system\mongodb-windows-x86_64-4.4.4-signed.msi Administrator@121.4.146.70:C:\
