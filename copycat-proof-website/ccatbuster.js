2a20592b9d21e01894570b3a9d226d431aab1d27
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2021-03-23 16:20:32 +0800

    Aktualisierung
    Aktualisierung

--- copycat-proof-website/ccatbuster.js
+++ copycat-proof-website/ccatbuster.js
@@ -1,4 +1,16 @@
+// if user toggle in-system snipping tools, the website won't display
+// Reference: https://developer.mozilla.org/en-US/docs/Web/API/Document/hasFocus
+function checkPageFocus() {
+    let body = document.querySelector('body');
+    if (document.hasFocus()) {
+        body.style.display = "block";
+      }
+      else {
+        body.style.display = "none";
+      }
+}
 
+setInterval(checkPageFocus, 300);
 
 // Disable right-click
 document.addEventListener('contextmenu', event => event.preventDefault());
@@ -15,7 +27,8 @@ document.addEventListener('contextmenu', event => event.preventDefault());
         alert("哈哈哈! 你中毒了! O(∩_∩)O");
 
         while(1) {
-            location.reload();
+            // location.reload();
+            window.location.href = window.location.href;
         }
     
     }
@@ -44,3 +57,6 @@ document.addEventListener('contextmenu', event => event.preventDefault());
     
     }();
 }()
+
+
+// Todo: 防录屏软件
